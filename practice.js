ae1f55eaab467c9fbde69752a66c59965591aa46
Kaleb Smith <93043410+iksmith@users.noreply.github.com>
2023-09-22 18:06:45 -0500

    first commit

---
+++ practice.js
@@ -0,0 +1,14 @@
+alert("Not waiting for JS");
+
+if (Number(age )< 18) {
+    alert("Sorry, you are too young to drive this car. Powering off");
+} else if 
+    (Number(age) === 18) {
+        alert("Congratulations on your first year of driving. Enjoy the ride!");
+    } else 
+        (Number(age) > 18); {
+            alert("Powering On. Enjoy the ride!")
+        };
+
+let age = prompt("How old are you?")
+
